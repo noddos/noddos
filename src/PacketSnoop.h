277d573ae27a85d5a49918cb478147300e349f80
Steven Hessing <steven.hessing@gmail.com>
2017-05-30 22:47:39 -0700

    A bit more DNS header parsing

--- src/PacketSnoop.h
+++ src/PacketSnoop.h
@@ -9,6 +9,7 @@
 #define PACKETSNOOP_H_
 
 #include <cstddef>
+#include <cstring>
 #include <syslog.h>
 #include <unistd.h>
 
@@ -18,9 +19,21 @@
 #include <linux/if_packet.h>
 #include <net/ethernet.h> /* the L2 protocols */
 #include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 
 #include "noddos.h"
 #include "iDeviceInfoSource.h"
+
+struct dnshdr {
+    uint16_t dns_id;
+    uint16_t dns_flags;
+    uint16_t dns_qdc;
+    uint16_t dns_anc;
+    uint16_t dns_nsc;
+    uint16_t dns_arc;
+};
 
 
 template <class T, std::size_t N>
@@ -40,11 +53,11 @@ public:
 	}
 
 	~PacketSnoop() { Close(); };
-	int PacketSnoop::Open(std::string input, uint32_t inExpiration = 0);
+	int Open(std::string input, uint32_t inExpiration = 0);
 	int GetFileHandle() { return sock; }
 	bool Close() { close (sock); return false; };
 	bool ProcessEvent(struct epoll_event &event) { return true; }
-	bool PacketSnoop::Parse (unsigned char *frame, size_t size);
+	bool Parse (unsigned char *frame, size_t size);
 	bool Parse_Dns_Tcp_Packet(unsigned char *payload, size_t size);
 	bool Parse_Dns_Udp_Packet(unsigned char *payload, size_t size);
 	bool Parse_Dhcp_Udp_Packet(unsigned char *payload, size_t size);
