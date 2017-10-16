34ef678f08c05f7d03fc8a4f1d1a7d60fba830b1
Steven Hessing <steven.hessing@gmail.com>
2017-10-15 21:23:03 -0700

    modify scripts and sample files yml

--- files/network.lua
+++
@@ -1,8 +0,0 @@
-        if nixio.fs.access("/etc/config/noddos") then
-            page = entry({"admin", "network", "noddos"}, cbi("admin_network/noddos"), nil)
-            page.target = cbi("admin_network/noddos")
-            page.title = _("Client Firewall")
-            page.order = 55
-            page.leaf = true
-        end
-
